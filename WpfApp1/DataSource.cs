855289c9c0b497182e72780267295b07560c4ef8
the.samn@gmail.com <the.samn@gmail.com>
2025-03-24 02:42:56 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/DataSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WpfApp1
+{
+    internal class DataSource
+    {
+        TaskCompletionSource<string>? tcs = null;
+        Window1 dataWindow;
+        public async Task<string> GetData()
+        {
+            dataWindow = new Window1();
+            tcs = new TaskCompletionSource<string>();
+            dataWindow.IsEnabled = true;
+            dataWindow.tcs = tcs;
+            dataWindow.Show();
+
+            await tcs.Task;
+
+            return tcs.Task.Result;
+        }
+    }
+}
